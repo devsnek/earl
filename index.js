74a96392234c5c7c72c85bfa9e56c18682b52606
Gus Caplan <me@gus.host>
2018-07-06 19:10:27 -0500

    even more perf

--- index.js
+++ index.js
@@ -3,18 +3,14 @@
 const Encoder = require('./src/encoder');
 const Decoder = require('./src/decoder');
 
-const kPackCustom = Symbol('earl.pack.custom');
-
 module.exports = {
   pack: (v) => {
     const encoder = new Encoder();
-    encoder.pack(v != null && v[kPackCustom] ? v[kPackCustom] : v);
-    return encoder.buffer.slice(0, encoder.offset);
+    encoder.pack(v);
+    return encoder.buffer.subarray(0, encoder.offset);
   },
   unpack: (v) => {
     const decoder = new Decoder(v);
     return decoder.unpack();
   },
 };
-
-module.exports.pack.custom = kPackCustom;
